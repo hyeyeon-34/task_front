550b614d72e4ed297463da96be51fe0c918e3d76
hyeyeon-34 <hyee092@gmail.com>
2024-09-19 12:49:53 +0900

    TEST4

--- src/utills/apiUrl.js
+++ src/utills/apiUrl.js
@@ -1,9 +1,9 @@
-const GET_TASKS_API_URL = 'https://taskback4.hyee34.site/get_tasks';
-const POST_TASKS_API_URL = 'https://taskback4.hyee34.site/post_tasks';
-const DELETE_TASKS_API_URL = 'https://taskback4.hyee34.site/delete_task';
+const GET_TASKS_API_URL = 'https://todoback3.hyee34.site/get_tasks';
+const POST_TASKS_API_URL = 'https://todoback3.hyee34.site/post_tasks';
+const DELETE_TASKS_API_URL = 'https://todoback3.hyee34.site/delete_task';
 const UPDATE_COMPLETED_TASKS_API_URL =
-  'https://taskback4.hyee34.site/update_completed_task';
-const UPDATE_TASK_API_URL = 'https://taskback4.hyee34.site/update_task';
+  'https://todoback3.hyee34.site/update_completed_task';
+const UPDATE_TASK_API_URL = 'https://todoback3.hyee34.site/update_task';
 export {
   GET_TASKS_API_URL,
   POST_TASKS_API_URL,
